38057e7b2d28ad9a2cbdf70a99bee11b946de228
Safwan Amani <safwanamanikps@gmail.com>
2021-03-10 12:38:46 +0530

    Connect the Mongodb and set up the routes

--- src/server.js
+++ src/server.js
@@ -1,8 +1,28 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
 const PORT = process.env.PORT;
 
 const app = express();
+
+//Routes
+const authRoutes = require('./routes/auth');
+
+//MongoDB Connection
+mongoose.connect(process.env.MONGO_URL + process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log("Database connected successfully");
+})
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/api", authRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is running port " + PORT);
